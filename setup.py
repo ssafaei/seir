e1d16b4e45e7c6d3ab4ca86953011ce7f3a955a2
Alan Garny <agarny@hellix.com>
2020-04-07 12:25:28 +1200

    Can now run the SEIR model from OpenCOR's Python Console window.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     author='Alan Garny',
     author_email='a.garny@auckland.ac.nz',
     description='OpenCOR-based Python script to model Covid-19 using the SEIR model',
+    scripts=['bin/seir', 'src/seir.py'],
     license='Apache 2.0',
     name='seir',
     url='https://github.com/ABI-Covid-19/seir',
